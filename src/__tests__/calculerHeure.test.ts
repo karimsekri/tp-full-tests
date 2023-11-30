ed97b4080cb18bc147f3e5b581f338e21292ff84
KarimSekri <karim.sekri@gmail.com>
2023-11-30 11:23:56 +0100

    init test unitaire

---
+++ src/__tests__/calculerHeure.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from 'vitest'
+import { calculerHeure, calculerHeureInt } from '../calculerHeure'
+
+describe('calculerHeure', () => {
+    describe('calculerHeureInt', () => {
+        test('calculerHeure', () => {
+            expect(calculerHeureInt(1, 2, 1)).toEqual(0)
+        });
+
+        test('calculerHeure', () => {
+            expect(calculerHeureInt(1, 2, 1)).toEqual(0)
+        });
+
+        test('calculerHeure', () => {
+            expect(calculerHeureInt(1, 2, 1)).toEqual(0)
+        });
+    });
+
+    describe('calculerHeure', () => {
+        
+    });
+
+});
---
+++ src/calculerHeure.ts
@@ -0,0 +1,14 @@
+export function calculerHeure(lune: number, terre: number, soleil: number): string {
+
+    return "mortin";
+}
+
+export function calculerHeureInt(lune: number, terre: number, soleil: number): number {
+    let resultat = 0;
+
+    if (lune === 1  && terre === 1 && soleil === 1) {
+        resultat = (1+1)-2
+    } 
+
+    return resultat
+}
